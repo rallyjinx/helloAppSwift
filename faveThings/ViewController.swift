843b9bdd5a0f5eb5ba17217a9df939d1388f32b6
Rally Jinx <rsjinx@gmail.com>
2017-02-22 16:26:03 -0600

    Initial Commit

---
+++ faveThings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  faveThings
+//
+//  Created by Jinx on 2/22/17.
+//  Copyright © 2017 Satan's Got Your Nose Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
